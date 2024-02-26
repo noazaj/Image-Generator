1247ddaa7aae33a54f1b376b5e2361d2f9c7af37
Noah Zajicek <nzajicek@WVWAUTHORITY.COM>
2024-02-26 12:02:58 -0500

    Update file

--- image_generator.py
+++ image_generator.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, send_from_directory, abort
 import random
 import os
 
@@ -9,14 +9,13 @@ IMAGES_DIR = 'imagesAll'
 
 @app.route('/random_character', methods=['GET'])
 def random_character():
-    selected = random.choice(metadata)
-    image_path = os.path.join(IMAGES_DIR, selected['file_name'])
-    image_url = f'http://localhost:5000/static/{selected["file_name"]}'
-    return jsonify({
-        'name': selected['name'],
-        'type': selected['type'],
-        'image_url': image_url,
-    })
-
+    try:
+        image_name = random.choice(os.listdir(IMAGES_DIR))
+        image_path = os.path.join(IMAGES_DIR, image_name)
+        return send_from_directory(IMAGES_DIR, image_name)
+    except Exception as err:
+        return abort(404)
 if __name__ == '__main__':
     app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
