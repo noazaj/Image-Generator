9f85943dfe1479c53331907104cc5f178e660ab1
Noah Zajicek <nzajicek@WVWAUTHORITY.COM>
2024-02-26 13:42:53 -0500

    Update files

--- image_generator.py
+++ image_generator.py
@@ -11,11 +11,6 @@ IMAGES_DIR = 'imagesAll'
 def random_character():
     try:
         image_name = random.choice(os.listdir(IMAGES_DIR))
-        image_path = os.path.join(IMAGES_DIR, image_name)
         return send_from_directory(IMAGES_DIR, image_name)
     except Exception as err:
         return abort(404)
-if __name__ == '__main__':
-    app.run(debug=True)
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
